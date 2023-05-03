a3506de9b691145cc6fa961d40c966ba47039c9b
Manling <110644990+MarlingHeli@users.noreply.github.com>
2023-05-04 09:35:46 +1200

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,14 @@
-import time
-import sys
+import time #for time delay
+import sys #for standard out
 
 def type(string):
-    for c in string:
-        sys.stdout.write(c)
-        sys.stdout.flush()
+#Prints letters one by one
+    for c in string: #for each character in the argument
+        sys.stdout.write(c) #display text
+        #sys.stdout.flush() immediately prints to console without output buffering
         time.sleep(0.1)
         
 type("\nHello, world!\n")
 time.sleep(0.2)
 type("\nI love programming :)")
-time.sleep(0.2)
+time.sleep(0.5)
